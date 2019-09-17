5d3c1b0039a896cbc39454c6e6f58b1202b9edfd
jjungs <jeongy95@naver.com>
2019-09-17 23:28:35 +0900

    server

---
+++ api.js
@@ -0,0 +1,34 @@
+const port = 80;
+
+let express = require('express');
+let app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile)
+
+app.get('/', (req, res) => {
+	res.render("index.html");
+});
+
+app.get('/html', (req, res) => {
+	res.send("<h1>저는 html 헤더입니다</h1>");
+});
+
+app.get('/json', (req, res) => {
+	let data = {
+		"data": "안녕하세요 " + req.query.name + "님",
+		"name": req.query.name
+	};
+	res.json(data);
+});
+
+app.get('/practice', (req, res) => {
+	res.render("practice.html");
+});
+
+let server = app.listen(port, () => {
+	console.log("Express server has started on port " + port);
+});
